e756cf93a4dd4432f6071367bbe18aad627dd166
drzzln <drzzln@protonmail.com>
2026-03-15 21:19:53 -0400

    refactor: use substrate repo-flake.nix universal builder

--- flake.nix
+++ flake.nix
@@ -3,37 +3,24 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    substrate = {
+      url = "github:pleme-io/substrate";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs { inherit system; };
-    in {
-      packages.default = pkgs.buildNpmPackage {
-        pname = "akeyless-github-action";
-        version = "0.0.0-dev";
-        src = self;
-        npmDepsHash = "sha256-0xATbadtb/oeX85Hhtn/OXnqiurk/lDX59a2Dzivj8E=";
-        NODE_OPTIONS = "--openssl-legacy-provider";
-        dontNpmBuild = false;
-        npmBuildScript = "package";
-        installPhase = ''
-          runHook preInstall
-          mkdir -p $out
-          cp -r dist $out/dist
-          cp action.yml $out/ 2>/dev/null || true
-          runHook postInstall
-        '';
-        meta = {
-          description = "Akeyless GitHub Action for fetching secrets and provisioning access";
-          homepage = "https://github.com/pleme-io/akeyless-github-action";
-          license = pkgs.lib.licenses.isc;
-        };
-      };
-
-      devShells.default = pkgs.mkShellNoCC {
-        packages = with pkgs; [ nodejs_22 ];
-      };
-    });
+  outputs = inputs: (import "${inputs.substrate}/lib/repo-flake.nix" {
+    inherit (inputs) nixpkgs flake-utils;
+  }) {
+    self = inputs.self;
+    language = "npm";
+    builder = "action";
+    pname = "akeyless-github-action";
+    npmDepsHash = "sha256-0xATbadtb/oeX85Hhtn/OXnqiurk/lDX59a2Dzivj8E=";
+    npmBuildScript = "package";
+    nodeOptions = "--openssl-legacy-provider";
+    description = "Akeyless GitHub Action for fetching secrets and provisioning access";
+    homepage = "https://github.com/pleme-io/akeyless-github-action";
+  };
 }
